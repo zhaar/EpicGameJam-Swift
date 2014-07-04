35ed71af6bc396e700c9874868d6ebbd599fdce3
zephyz <zephyz.zhaar@gmail.com>
2014-07-04 22:57:59 +0200

    changed orientation

--- EPICGAMETEST/GameScene.swift
+++ EPICGAMETEST/GameScene.swift
@@ -11,6 +11,7 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        self.userInteractionEnabled = true
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
         myLabel.text = "Hello, World!";
         myLabel.fontSize = 65;
@@ -24,15 +25,14 @@ class GameScene: SKScene {
         
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            let sprite = SKSpriteNode(imageNamed:"Screen Shot 2014-07-04 at 21.24.46")
             
             sprite.xScale = 0.5
             sprite.yScale = 0.5
-            sprite.position = location
+            sprite.position = CGPoint(x:200, y:200)
             
             let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
+           
             sprite.runAction(SKAction.repeatActionForever(action))
             
             self.addChild(sprite)
--- EPICGAMETEST/prototyping.playground/section-1.swift
+++ EPICGAMETEST/prototyping.playground/section-1.swift
@@ -1,5 +1,9 @@
 // Playground - noun: a place where people can play
 
 import UIKit
+import SpriteKit
 
 let str = "Hello, playground"
+let sprite = SKSpriteNode(imageNamed: "Screen Shot 2014-07-04 at 21.24.46")
+sprite.position = CGPoint(x:0, y:0)
+sprite.anchorPoint = CGPoint(x:0.5,y:0.5)
